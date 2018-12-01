102030bfd3bfd2801961d0a1a6f2b7b906633b95
Thierry Carrard <thierry.carrard@cea.fr>
2018-12-01 23:01:46 +0100

    test split into two tests

--- tests/soatest.cpp
+++ tests/soatest.cpp
@@ -14,38 +14,18 @@
 
 std::default_random_engine rng;
 
-static inline void compute_distance( double& dist, double x, double y, double z, double x2, double y2, double z2 )
-{
-	x = x - x2;
-	y = y - y2;
-	z = z - z2;
-	dist = std::exp(x2+y2+z2) / std::sqrt( x*x + y*y + z*z );
-}
-
-template<typename OperatorT, typename... ArrayP>
-static inline void apply_to_arrays( OperatorT f, size_t first, size_t N, ArrayP __restrict__ ... arraypack )
-{
-	size_t last = first+N;
-	#pragma omp simd
-	for(size_t i=first;i<last;i++)
-	{
-		f( arraypack[i] ... );
-	}
-}
-
 // WARNING: assumes that elements in arrays support 'operator = (const size_t&)' and 'operator == (const size_t&)'
 template<typename FieldsT, size_t... _ids>
 static inline void check_field_arrays_aliasing( size_t N, FieldsT& field_arrays, const soatl::FieldId<_ids>& ... fids )
 {
 	std::uniform_int_distribution<> rndist(0,N*2);
 
-	/*
+#ifdef DEBUG
 	std::cout<<"check_field_arrays_aliasing: align="<<field_arrays.alignment()<<", chunksize="<<field_arrays.chunksize()<<", using fields :"<<std::endl;
 	TEMPLATE_LIST_BEGIN
 		std::cout<<"\t"<< soatl::FieldDescriptor<_ids>::name() <<std::endl
 	TEMPLATE_LIST_END
-	*/
-
+#endif
         for(size_t j=1;j<=N;j++)
         {
                 field_arrays.resize(j);
@@ -56,7 +36,7 @@ static inline void check_field_arrays_aliasing( size_t N, FieldsT& field_arrays,
 			void* ptr;
 			size_t addr;
 			TEMPLATE_LIST_BEGIN
-				ptr = field_arrays.get( fids ) ,
+				ptr = field_arrays[fids] ,
 				addr = reinterpret_cast<size_t>(ptr) ,
 				assert( ( addr % alignment ) == 0 ) 
 			TEMPLATE_LIST_END
@@ -75,7 +55,7 @@ static inline void check_field_arrays_aliasing( size_t N, FieldsT& field_arrays,
 		for(size_t i=0;i<j;i++)
                 {
 			TEMPLATE_LIST_BEGIN
-				field_arrays.get( fids )[i] = static_cast<typename soatl::FieldDescriptor<_ids>::value_type>( k ) ,
+				field_arrays[fids][i] = static_cast<typename soatl::FieldDescriptor<_ids>::value_type>( k ) ,
 				++k
 			TEMPLATE_LIST_END
 		}
@@ -83,8 +63,8 @@ static inline void check_field_arrays_aliasing( size_t N, FieldsT& field_arrays,
 		for(size_t i=j;i<field_arrays.chunk_ceil();i++)
                 {
 			TEMPLATE_LIST_BEGIN
-				k += static_cast<size_t>( field_arrays.get( fids )[i] ) ,
-				field_arrays.get( fids )[i] = static_cast<typename soatl::FieldDescriptor<_ids>::value_type>( k ) 
+				k += static_cast<size_t>( field_arrays[fids][i] ) ,
+				field_arrays[fids][i] = static_cast<typename soatl::FieldDescriptor<_ids>::value_type>( k ) 
 			TEMPLATE_LIST_END
 		}
 
@@ -95,8 +75,8 @@ static inline void check_field_arrays_aliasing( size_t N, FieldsT& field_arrays,
 			bool value_ok = false;
 			size_t findex = 0;
 			TEMPLATE_LIST_BEGIN
-				value_ok = field_arrays.get( fids )[i] == static_cast<typename soatl::FieldDescriptor<_ids>::value_type>( k ) ,
-				//std::cout<<"value["<<findex<<"]="<< (size_t)(field_arrays.get( FieldDescriptors() )[i] )<<std::endl ,
+				value_ok = field_arrays[fids][i] == static_cast<typename soatl::FieldDescriptor<_ids>::value_type>( k ) ,
+				//std::cout<<"value["<<findex<<"]="<< (size_t)(field_arrays[fids][i] )<<std::endl ,
 				assert(value_ok) ,
 				++ findex ,
 				++k
@@ -105,17 +85,21 @@ static inline void check_field_arrays_aliasing( size_t N, FieldsT& field_arrays,
 		
 		// test that a resize keeps values
 		size_t ns = rndist(rng);
-		field_arrays.resize( ns );
 		size_t cs = std::min( ns , j );
-		//std::cout<<"resize from "<<j<<" to "<<ns<<", cs="<<cs<< std::endl;
+#ifdef DEBUG
+		std::cout<<"resize from "<<j<<" to "<<ns<<", cs="<<cs<< std::endl;
+#endif
+		field_arrays.resize( ns );
 		k=0;
 		for(size_t i=0;i<cs;i++)
                 {
 			bool value_ok = false;
 			size_t findex = 0;
 			TEMPLATE_LIST_BEGIN
-				value_ok = field_arrays.get( fids )[i] == static_cast<typename soatl::FieldDescriptor<_ids>::value_type>( k ) ,
-				//std::cout<<"value["<<findex<<"]="<< (size_t)( field_arrays.get( FieldDescriptors() )[i] )<<std::endl ,
+				value_ok = field_arrays[fids][i] == static_cast<typename soatl::FieldDescriptor<_ids>::value_type>( k ) ,
+#ifdef DEBUG
+				std::cout<<"value["<<_ids<<"]["<<i<<"] ="<< (size_t)( field_arrays[fids][i] )<<std::endl ,
+#endif
 				assert(value_ok) ,
 				++ findex ,
 				++k
@@ -130,8 +114,10 @@ static inline void check_field_arrays_aliasing( size_t N, FieldsT& field_arrays,
 		void* ptr;
 		size_t findex = 0;
 		TEMPLATE_LIST_BEGIN
-			ptr = field_arrays.get( fids ) ,
-			//std::cout<<"ptr["<<findex<<"]="<<ptr<<std::endl ,
+			ptr = field_arrays[fids] ,
+#ifdef DEBUG
+			std::cout<<"ptr["<<findex<<"]="<<ptr<<std::endl ,
+#endif
 			++findex , 
 			assert(ptr==nullptr) 
 		TEMPLATE_LIST_END
@@ -182,7 +168,7 @@ static inline void test_packed_field_arrays_aliasing()
 template<size_t A, size_t C>
 static inline void test_field_arrays_aliasing()
 {
-	std::cout<<"test_field_arrays_aliasing<"<<C<<">"<<std::endl;	
+	std::cout<<"test_field_arrays_aliasing<"<<A<<","<<C<<">"<<std::endl;	
 
 	auto rx = particle_rx;
 	auto ry = particle_ry;
@@ -265,89 +251,6 @@ int main(int argc, char* argv[])
 	test_field_arrays_aliasing<64,3>();
 	test_field_arrays_aliasing<64,6>();
 	test_field_arrays_aliasing<64,16>();
-
-	auto rx = particle_rx;
-	auto ry = particle_ry;
-	auto rz = particle_rz;
-	auto e = particle_e;
-	auto atype = particle_atype;
-	auto mid = particle_mid;
-	auto tmp1 = particle_tmp1;
-	auto tmp2 = particle_tmp2;
-	auto dist = particle_dist;
-
-	auto cell_arrays1 = soatl::make_field_arrays( rx,ry,rz,e,dist );
-	auto cell_arrays2 = soatl::make_packed_field_arrays( soatl::cst::align<64>(), soatl::cst::chunk<8>(), atype,rx,mid,ry,rz );
-
-	//check_field_arrays_aliasing(N,cell_arrays2 , atype,rx,mid,ry,rz );
-
-	// rebind operator ?
-	// zip operator ?
-	// embed several field_arrays ?
-
-	// zip arrays
-	cell_arrays1.resize(N);
-	cell_arrays2.resize(N);
-
-	double* __restrict__ rx_ptr = static_cast<double*>( __builtin_assume_aligned( cell_arrays1.get(rx) , 64 ) );
-	double* __restrict__ ry_ptr = static_cast<double*>( __builtin_assume_aligned( cell_arrays1.get(ry) , 64 ) );
-	double* __restrict__ rz_ptr = static_cast<double*>( __builtin_assume_aligned( cell_arrays1.get(rz) , 64 ) );
-	double* __restrict__ rx2_ptr = static_cast<double*>( __builtin_assume_aligned( cell_arrays2.get(rx) , 64 ) );
-	double* __restrict__ ry2_ptr = static_cast<double*>( __builtin_assume_aligned( cell_arrays2.get(ry) , 64 ) );
-	double* __restrict__ rz2_ptr = static_cast<double*>( __builtin_assume_aligned( cell_arrays2.get(rz) , 64 ) );
-	double* __restrict__ e_ptr = static_cast<double*>( __builtin_assume_aligned( cell_arrays1.get(e) , 64 ) );
-	unsigned char* __restrict__ at_ptr = static_cast<unsigned char*>( __builtin_assume_aligned( cell_arrays2.get(atype) , 64 ) );
-	int* __restrict__ m_ptr = static_cast<int*>( __builtin_assume_aligned( cell_arrays2.get(mid) , 64 ) );
-	double* __restrict__ dist_ptr = static_cast<double*>( __builtin_assume_aligned( cell_arrays1.get(dist) , 64 ) );
-
-	std::uniform_real_distribution<> rdist(0.0,1.0);
-
-	for(size_t i=0;i<N;i++)
-	{
-		rx_ptr[i] = rdist(rng);
-		ry_ptr[i] = rdist(rng);
-		rz_ptr[i] = rdist(rng);
-		rx2_ptr[i] = rdist(rng);
-		ry2_ptr[i] = rdist(rng);
-		rz2_ptr[i] = rdist(rng);
-		e_ptr[i] = rdist(rng);
-		at_ptr[i] = static_cast<unsigned int>( rdist(rng)*50 );
-		m_ptr[i] = static_cast<unsigned int>( at_ptr[i] + rdist(rng)*500 );
-		dist_ptr[i] = static_cast<unsigned int>( at_ptr[i] + rdist(rng)*500 );
-	}
-	
-	apply_to_arrays( compute_distance , 0, N, dist_ptr, rx_ptr, ry_ptr, rz_ptr, rx2_ptr, ry2_ptr, rz2_ptr );
-	//apply_to_field_arrays( compute_distance , 0, N, cell_arrays1 );
-
-	for(size_t j=0;j<N;j++)
-	{
-		std::cout<<"dist["<<j<<"]="<<dist_ptr[j]<<std::endl;
-	}
-
-
-	auto zip = soatl::make_field_pointers( N, soatl::cst::align<64>(), soatl::cst::chunk<8>(), rx, ry, rz, dist );
-
-	zip.get(rx) = cell_arrays2.get(rx);
-	zip.get(ry) = cell_arrays2.get(ry);
-	zip.get(rz) = cell_arrays2.get(rz);
-	zip.get(dist) = cell_arrays1.get(dist);
-
-	soatl::copy( zip , cell_arrays1 , 0, N, rx, ry, rz );
-
-	double ax = rx2_ptr[0];
-	double ay = ry2_ptr[0];
-	double az = rz2_ptr[0];
-
-	apply_to_arrays( [ax,ay,az](float& d, double x, double y, double z)
-			 {
-			     x -= ax;
-			     y -= ay;
-			     z -= az;
-			     d = std::sqrt(x*x+y*y+z*z);
-			 }
-			 , 0, N,
-			 zip.get(dist), zip.get(rx), zip.get(ry), zip.get(rz)
-			 );
 }
 
 
